92e3fd01758cef13f6bd2e6a798b008bcb1aab3a
Yasin AKYAR <moshimsc_06@hotmail.com>
2022-10-03 18:07:02 +0300

    push slider

--- src/components/About.js
+++ src/components/About.js
@@ -7,8 +7,19 @@ export default function About() {
         <div className='oppa1'>About</div>
         <div className='oppa2'>About Me</div>
       </div>
-      
-      
+      <div className='page'>
+        <div className='pictures'>
+          <div className='pic1'><img src="/logo/p1.jpg" /></div>
+          <div className='pic2'></div>
+        </div>
+        <div className='text'>
+          <div className='tı1'><p className='pı1'> My name is </p> <p className='pı2'>Aylin KARADAVUT</p></div>
+          <div className='tı3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero rem sunt et culpa. Dicta id minus ducimus sed. Soluta explicabo facere voluptatem inventore natus iusto eius iste repellendus perferendis? Possimus tempore veritatis, omnis ducimus tempora ad id illum exercitationem consectetur, dicta ipsa, dolore reprehenderit soluta. Fuga laborum pariatur in corrupti voluptatem odio, enim tenetur quas dignissimos labore error, blanditiis, accusantium similique aut quia beatae ipsa esse aliquid consequuntur asperiores facere.</div>
+        </div>
+
+      </div>
+
+
     </div>
   )
 }
